0f84fc663c7e425d45569fcfeb508466235b7a17
sauravnakarmi <sauravnakarmi@gmail.com>
2025-02-23 16:30:26 -0600

    implemented unity 2d platformer for beginners jumping animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,312 +1,49 @@
-/*
-	Created by @DawnosaurDev at youtube.com/c/DawnosaurStudios
-	Thanks so much for checking this out and I hope you find it helpful! 
-	If you have any further queries, questions or feedback feel free to reach out on my twitter or leave a comment on youtube :D
-
-	Feel free to use this in your own games, and I'd love to see anything you make!
- */
-
-using System.Collections;
 using UnityEngine;
-
+ 
 public class PlayerMovement : MonoBehaviour
 {
-	//Scriptable object which holds all the player's movement parameters. If you don't want to use it
-	//just paste in all the parameters, though you will need to manuly change all references in this script
-
-	//HOW TO: to add the scriptable object, right-click in the project window -> create -> Player Data
-	//Next, drag it into the slot in playerMovement on your player
-
-	public PlayerData Data;
-
-	private bool grounded;
-
-	#region Variables
-	//Components
-    public Rigidbody2D RB { get; private set; }
-
-	//Variables control the various actions the player can perform at any time.
-	//These are fields which can are public allowing for other sctipts to read them
-	//but can only be privately written to.
-	public bool IsFacingRight { get; private set; }
-	public bool IsJumping { get; private set; }
-	public bool IsSliding { get; private set; }
-
-	//Timers (also all fields, could be private and a method returning a bool could be used)
-	public float LastOnGroundTime { get; private set; }
-
-	//Jump
-	private bool _isJumpCut;
-	private bool _isJumpFalling;
-
-	
-
-	private Vector2 _moveInput;
-	public float LastPressedJumpTime { get; private set; }
-
-	//Set all of these up in the inspector
-	[Header("Checks")] 
-	[SerializeField] private Transform _groundCheckPoint;
-	//Size of groundCheck depends on the size of your character generally you want them slightly small than width (for ground) and height (for the wall check)
-	[SerializeField] private Vector2 _groundCheckSize = new Vector2(0.49f, 0.03f);
-	[Space(5)]
-
-    [Header("Layers & Tags")]
-	[SerializeField] private LayerMask _groundLayer;
-	#endregion
-
+    [SerializeField] private float speed;
+    [SerializeField] private float size;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+    
     private void Awake()
-	{
-		RB = GetComponent<Rigidbody2D>();
-	}
-
-	private void Start()
-	{
-		SetGravityScale(Data.gravityScale);
-		IsFacingRight = true;
-	}
-
-	private void Update()
-	{
-        #region TIMERS
-        LastOnGroundTime -= Time.deltaTime;
-
-		LastPressedJumpTime -= Time.deltaTime;
-		#endregion
-
-		#region INPUT HANDLER
-		_moveInput.x = Input.GetAxisRaw("Horizontal");
-		_moveInput.y = Input.GetAxisRaw("Vertical");
-
-		if (_moveInput.x != 0)
-			CheckDirectionToFace(_moveInput.x > 0);
-
-		if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.J))
-        {
-			OnJumpInput();
-        }
-
-		if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.C) || Input.GetKeyUp(KeyCode.J))
-		{
-			OnJumpUpInput();
-		}
-		#endregion
-
-		#region COLLISION CHECKS
-		if (!IsJumping)
-		{
-			//Ground Check
-			if (Physics2D.OverlapBox(_groundCheckPoint.position, _groundCheckSize, 0, _groundLayer) && !IsJumping) //checks if set box overlaps with ground
-			{
-				LastOnGroundTime = Data.coyoteTime; //if so sets the lastGrounded to coyoteTime
-            }		
-
-
-			
-		}
-		#endregion
-
-		#region JUMP CHECKS
-		if (IsJumping && RB.linearVelocity.y < 0)
-		{
-			IsJumping = false;
-		}
-
-		//Jump
-		if (CanJump() && LastPressedJumpTime > 0)
-		{
-			IsJumping = true;
-			_isJumpCut = false;
-			_isJumpFalling = false;
-			grounded = false;
-			Jump();
-		}
-
-
-
-		#region GRAVITY
-		//Higher gravity if we've released the jump input or are falling
-		if (IsSliding)
-		{
-			SetGravityScale(0);
-		}
-		else if (RB.linearVelocity.y < 0 && _moveInput.y < 0)
-		{
-			//Much higher gravity if holding down
-			SetGravityScale(Data.gravityScale * Data.fastFallGravityMult);
-			//Caps maximum fall speed, so when falling over large distances we don't accelerate to insanely high speeds
-			RB.linearVelocity = new Vector2(RB.linearVelocity.x, Mathf.Max(RB.linearVelocity.y, -Data.maxFastFallSpeed));
-		}
-		else if (_isJumpCut)
-		{
-			//Higher gravity if jump button released
-			SetGravityScale(Data.gravityScale * Data.jumpCutGravityMult);
-			RB.linearVelocity = new Vector2(RB.linearVelocity.x, Mathf.Max(RB.linearVelocity.y, -Data.maxFallSpeed));
-		}
-		else if ((IsJumping || _isJumpFalling) && Mathf.Abs(RB.linearVelocity.y) < Data.jumpHangTimeThreshold)
-		{
-			SetGravityScale(Data.gravityScale * Data.jumpHangGravityMult);
-		}
-		else if (RB.linearVelocity.y < 0)
-		{
-			//Higher gravity if falling
-			SetGravityScale(Data.gravityScale * Data.fallGravityMult);
-			//Caps maximum fall speed, so when falling over large distances we don't accelerate to insanely high speeds
-			RB.linearVelocity = new Vector2(RB.linearVelocity.x, Mathf.Max(RB.linearVelocity.y, -Data.maxFallSpeed));
-		}
-		else
-		{
-			//Default gravity if standing on a platform or moving upwards
-			SetGravityScale(Data.gravityScale);
-		}
-		#endregion
+    {   
+        //Grabs references for rigidbody and animator from game object.
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
-
-    private void FixedUpdate()
-	{
-			Run(1);
+ 
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+ 
+        //Flip player when facing left/right.
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector2(1*size, 1*size);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector2(-1*size, 1*size);
+ 
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+ 
+        //sets animation parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
     }
-
-    #region INPUT CALLBACKS
-	//Methods which whandle input detected in Update()
-    public void OnJumpInput()
-	{
-		LastPressedJumpTime = Data.jumpInputBufferTime;
-	}
-
-	public void OnJumpUpInput()
-	{
-		if (CanJumpCut())
-			_isJumpCut = true;
-	}
-    #endregion
-
-    #region GENERAL METHODS
-    public void SetGravityScale(float scale)
-	{
-		RB.gravityScale = scale;
-	}
-    #endregion
-
-	//MOVEMENT METHODS
-    #region RUN METHODS
-    private void Run(float lerpAmount)
-	{
-		//Calculate the direction we want to move in and our desired velocity
-		float targetSpeed = _moveInput.x * Data.runMaxSpeed;
-		//We can reduce are control using Lerp() this smooths changes to are direction and speed
-		targetSpeed = Mathf.Lerp(RB.linearVelocity.x, targetSpeed, lerpAmount);
-
-		#region Calculate AccelRate
-		float accelRate;
-
-		//Gets an acceleration value based on if we are accelerating (includes turning) 
-		//or trying to decelerate (stop). As well as applying a multiplier if we're air borne.
-		if (LastOnGroundTime > 0)
-			accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? Data.runAccelAmount : Data.runDeccelAmount;
-		else
-			accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? Data.runAccelAmount * Data.accelInAir : Data.runDeccelAmount * Data.deccelInAir;
-		#endregion
-
-		#region Add Bonus Jump Apex Acceleration
-		//Increase are acceleration and maxSpeed when at the apex of their jump, makes the jump feel a bit more bouncy, responsive and natural
-		if ((IsJumping || _isJumpFalling) && Mathf.Abs(RB.linearVelocity.y) < Data.jumpHangTimeThreshold)
-		{
-			accelRate *= Data.jumpHangAccelerationMult;
-			targetSpeed *= Data.jumpHangMaxSpeedMult;
-		}
-		#endregion
-
-		#region Conserve Momentum
-		//We won't slow the player down if they are moving in their desired direction but at a greater speed than their maxSpeed
-		if(Data.doConserveMomentum && Mathf.Abs(RB.linearVelocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(RB.linearVelocity.x) == Mathf.Sign(targetSpeed) && Mathf.Abs(targetSpeed) > 0.01f && LastOnGroundTime < 0)
-		{
-			//Prevent any deceleration from happening, or in other words conserve are current momentum
-			//You could experiment with allowing for the player to slightly increae their speed whilst in this "state"
-			accelRate = 0; 
-		}
-		#endregion
-
-		//Calculate difference between current velocity and desired velocity
-		float speedDif = targetSpeed - RB.linearVelocity.x;
-		//Calculate force along x-axis to apply to thr player
-
-		float movement = speedDif * accelRate;
-
-		//Convert this to a vector and apply to rigidbody
-		RB.AddForce(movement * Vector2.right, ForceMode2D.Force);
-
-		/*
-		 * For those interested here is what AddForce() will do
-		 * RB.velocity = new Vector2(RB.velocity.x + (Time.fixedDeltaTime  * speedDif * accelRate) / RB.mass, RB.velocity.y);
-		 * Time.fixedDeltaTime is by default in Unity 0.02 seconds equal to 50 FixedUpdate() calls per second
-		*/
-	}
-	#endregion
-
-	private void Turn()
-	{
-		//stores scale and flips the player along the x axis, 
-		Vector3 scale = transform.localScale; 
-		scale.x *= -1;
-		transform.localScale = scale;
-
-		IsFacingRight = !IsFacingRight;
-	}
-    #endregion
-
-    #region JUMP METHODS
+ 
     private void Jump()
-	{
-		//Ensures we can't call Jump multiple times from one press
-		LastPressedJumpTime = 0;
-		LastOnGroundTime = 0;
-
-		#region Perform Jump
-		//We increase the force applied if we are falling
-		//This means we'll always feel like we jump the same amount 
-		//(setting the player's Y velocity to 0 beforehand will likely work the same, but I find this more elegant :D)
-		float force = Data.jumpForce;
-		if (RB.linearVelocity.y < 0)
-			force -= RB.linearVelocity.y;
-
-		RB.AddForce(Vector2.up * force, ForceMode2D.Impulse);
-		#endregion
-	}
-
-
-    #region CHECK METHODS
-    public void CheckDirectionToFace(bool isMovingRight)
-	{
-		if (isMovingRight != IsFacingRight)
-			Turn();
-	}
-
-    private bool CanJump()
     {
-		return LastOnGroundTime > 0 && !IsJumping && grounded;
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
     }
-
-	private bool CanJumpCut()
+ 
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-		return IsJumping && RB.linearVelocity.y > 0 && grounded;
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
-	#endregion
-
-    #region EDITOR METHODS
-    private void OnDrawGizmosSelected()
-    {
-		Gizmos.color = Color.green;
-		Gizmos.DrawWireCube(_groundCheckPoint.position, _groundCheckSize);
-		Gizmos.color = Color.blue;
-	}
-    #endregion
-
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-		if(collision.gameObject.tag == "Ground")
-			grounded = true;
-	}
 }
-#endregion
-// created by Dawnosaur :D
