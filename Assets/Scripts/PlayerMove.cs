f0fd300c76ef629b402790d6fc3489acf8e221ee
sauravnakarmi <sauravnakarmi@gmail.com>
2025-02-23 00:31:13 -0600

    2dPlayerMovementScript

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,16 +2,20 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    private Rigidbody2D body;
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D body;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        body.linearVelocity = new Vector3(Input.GetAxis("Horizontal"), body.linearVelocity.y, 0);
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
     }
 }
