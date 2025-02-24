4d40fb0e1b1089700d7f73e189d4e8e834ad6def
Maria Liu <marialiu0220@gmail.com>
2025-02-24 18:41:12 -0500

    feat: add basic npc dialogue box

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public string[] dialogueLines;
+    private int index = 0;
+    private bool isPlayerInRange = false;
+    public float typeSpeed = 0.05f;
+    public GameObject contButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerInRange)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                ResetText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+
+        if (dialogueText.text == dialogueLines[index])
+        {
+            contButton.SetActive(true);
+        }
+    }
+
+    private void ResetText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogueLines[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        contButton.SetActive(false);
+
+        if (index < dialogueLines.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            ResetText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            print("Player is in range of NPC");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            // print statement
+            print("Player is no longer in range of NPC");
+            ResetText();
+        }
+    }
+}
