c1814df5884fa9ff18225532aac44c5e2c60e3f3
Maria Liu <marialiu0220@gmail.com>
2025-03-27 20:43:17 -0400

    feat: add points to person based on pokemon choice

--- Assets/Dialogue/globals.ink
+++ Assets/Dialogue/globals.ink
@@ -1,1 +1,4 @@
 VAR pokemonName = ""
+VAR vicky = 0
+VAR vivian = 0
+VAR benny = 0
--- Assets/Scripts/Dialogue/DialogueVariables.cs
+++ Assets/Scripts/Dialogue/DialogueVariables.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ink.Runtime;
-using System.IO;
-using System;
 
 public class DialogueVariables
 {
@@ -41,8 +39,9 @@ public class DialogueVariables
         // only maintain variables that were initialized from the globals ink file
         if (variables.ContainsKey(name))
         {
+            CalculatePoints(value.ToString());
             variables[name] = value;
-            Debug.Log("Update variable: " + name + " = " + value);
+            PrintVariables();
         }
     }
 
@@ -51,6 +50,42 @@ public class DialogueVariables
         foreach (KeyValuePair<string, Ink.Runtime.Object> variable in variables)
         {
             story.variablesState.SetGlobal(variable.Key, variable.Value);
+        }
+    }
+
+    private void CalculatePoints(string choice)
+    {
+        switch (choice)
+        {
+            case "Charmander":
+                if (variables["vicky"] is Ink.Runtime.IntValue vickyInt)
+                {
+                    variables["vicky"] = new Ink.Runtime.IntValue(vickyInt.value + 1);
+                }
+                break;
+            case "Squirtle":
+                if (variables["vivian"] is Ink.Runtime.IntValue vivInt)
+                {
+                    variables["vivian"] = new Ink.Runtime.IntValue(vivInt.value + 1);
+                }
+                break;
+            case "Bulbasaur":
+                if (variables["benny"] is Ink.Runtime.IntValue bennyInt)
+                {
+                    variables["benny"] = new Ink.Runtime.IntValue(bennyInt.value + 1);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    // for debugging
+    public void PrintVariables()
+    {
+        foreach (var variable in variables)
+        {
+            Debug.Log(variable.Key + " = " + variable.Value);
         }
     }
 
